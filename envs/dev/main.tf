7258545fd3fd779062fb2db8624084e514095273
sotoiwa <sotoiwa@gmail.com>
2022-08-24 02:35:55 +0900

    fix logging

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -4,13 +4,12 @@ module "network" {
 }
 
 module "bastion" {
-  source                                 = "../../modules/bastion"
-  app_name                               = var.app_name
-  vpc_id                                 = module.network.vpc_id
-  private_subnet_a_id                    = module.network.private_subnet_a_id
-  private_subnet_c_id                    = module.network.private_subnet_c_id
-  session_manager_logging_log_group_name = var.session_manager_logging_log_group_name
-  session_manager_logging_bucket_name    = var.session_manager_logging_bucket_name
+  source                              = "../../modules/bastion"
+  app_name                            = var.app_name
+  vpc_id                              = module.network.vpc_id
+  private_subnet_a_id                 = module.network.private_subnet_a_id
+  private_subnet_c_id                 = module.network.private_subnet_c_id
+  session_manager_logging_bucket_name = var.session_manager_logging_bucket_name
 }
 
 module "database" {
--- envs/dev/variables.tf
+++ envs/dev/variables.tf
@@ -6,6 +6,4 @@ variable "db_master_username" {}
 
 variable "db_master_password" {}
 
-variable "session_manager_logging_log_group_name" {}
-
 variable "session_manager_logging_bucket_name" {}
