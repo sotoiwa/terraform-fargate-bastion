1b8c3e5a1ed4b36b99e334737c3e8972e59d4c66
sotoiwa <sotoiwa@gmail.com>
2022-08-23 22:55:54 +0900

    fix subnet setting

--- modules/network/aws_subnet.tf
+++ modules/network/aws_subnet.tf
@@ -2,7 +2,7 @@ resource "aws_subnet" "private_a" {
   vpc_id                  = aws_vpc.this.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "${data.aws_region.this.name}a"
-  map_public_ip_on_launch = true
+  map_public_ip_on_launch = false
 
   tags = {
     Name = "${var.app_name}-private-subnet-a"
@@ -13,7 +13,7 @@ resource "aws_subnet" "private_c" {
   vpc_id                  = aws_vpc.this.id
   cidr_block              = "10.0.2.0/24"
   availability_zone       = "${data.aws_region.this.name}c"
-  map_public_ip_on_launch = true
+  map_public_ip_on_launch = false
 
   tags = {
     Name = "${var.app_name}-private-subnet-c"
